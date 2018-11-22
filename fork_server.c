56628f1ce18e8727f8ef52f81b8acca6aeb7a57e
chihyu <ccyu1206@gmail.com>
2018-11-22 13:01:24 +0800

    initial

---
+++ fork_server.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+#define PORT "80"
+#define RESTRICT 10
+
+char website[] = 
+"HTTP/1.1 200 OK\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+"<!DOCTYPE html>\r\n"
+"<html>\r\n"
+"<head>\r\n"
+"<meta name=\"viewport\" content=\"width=device-width, minimum-scale=0.1\">\r\n"
+"<title>0Dw4dCP.png (1200×1678)</title></head><body style=\"margin: 0px; background: #0e0e0e;\">\r\n"
+"<img style=\"-webkit-user-select: none;cursor: zoom-in;\" src=\"https://i.imgur.com/0Dw4dCP.png\" width=\"379\" height=\"530\">\r\n"
+"</body></html>\r\n"; /*html website*/
+
+void sigchld_handler(int s)
+{
+  while (waitpid(-1, NULL, WNOHANG) > 0)
+    ;
+}
+
+int main(int argc, char *argv[]) {
+	struct sockaddr_in server_addr, client_addr;  /*server && client's address*/
+	socklen_t addr_len = sizeof(client_addr);
+	int server_fd, client_fd;
+	char buf[4096];                /*use to store infomation from client*/
+
+  int status;
+  struct addrinfo hints;
+  struct addrinfo *servinfo, *ptr; // point to the result and a addrinfo pointer
+  memset(&hints, 0, sizeof(hints)); // ensure the struct is empty
+  hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE; // fill up my IP 
+  if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+  int optval = 1;
+  for (ptr = servinfo; ptr != NULL; ptr = ptr->ai_next) {  //run a for loop to chect which is able to use
+    if ((server_fd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) == -1) { //set socket
+      perror("server: socket");
+      continue;
+    }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1) { //set socket option
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if (bind(server_fd, ptr->ai_addr, ptr->ai_addrlen) == -1) { //set port number to defined PORT in getaddrinfo 
+      perror("server: bind");
+      close(server_fd);
+      continue;
+    }
+    break;
+  }
+  if (ptr == NULL) {  //nothing is eligible
+    fprintf(stderr, "server: failed to bind\n");
+    return 2;
+  }
+  freeaddrinfo(servinfo); // 全部都用這個 structure
+  if (listen(server_fd, RESTRICT) == -1) {  //listen at port 80, the limit is RESTRICT(a constant)
+    perror("listen");
+    exit(1);
+  }
+  /*deal with the zombie process using the waitpid(),
+  then the parent process have to wait until the child process end.
+  Thus, the kernel can get the info and release the pid*/
+  struct sigaction sa;             
+  sa.sa_handler = sigchld_handler;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+    perror("sigaction");
+    exit(1);
+  }
+
+  printf("Server: Waiting for connections...\n");
+  /*main loop to accept*/
+  int pid;
+	while(1) {
+		client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len);
+
+		if(client_fd == -1) {
+			perror("Connection failed\n");
+			continue;
+		}
+    printf("Got client connection from %s\n", inet_ntoa(client_addr.sin_addr));
+		pid = fork();
+		if (pid == 0) {
+			/*child process */
+			close(server_fd);
+			memset(buf, '\0', sizeof(buf));
+			read(client_fd, buf, sizeof(buf) - 1);
+
+			printf("%s\n", buf);
+
+			write(client_fd, website, sizeof(website) -1);
+      close (client_fd);
+			printf("Closing the client_fd\n");
+			exit(0);
+		}
+		/*parent process*/
+		if (pid > 0) {
+			wait(NULL);
+			close(client_fd);
+		}
+	}
+	return 0;
+}
---
+++ select_server.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define PORT "80"
+#define RESTRICT 10
+
+char website[] = 
+"HTTP/1.1 200 OK\r\n"
+"Content-Type: text/html; charset=UTF-8\r\n\r\n"
+"<!DOCTYPE html>\r\n"
+"<html>\r\n"
+"<head>\r\n"
+"<meta name=\"viewport\" content=\"width=device-width, minimum-scale=0.1\">\r\n"
+"<title>0Dw4dCP.png (1200×1678)</title></head><body style=\"margin: 0px; background: #0e0e0e;\">\r\n"
+"<img style=\"-webkit-user-select: none;cursor: zoom-in;\" src=\"https://i.imgur.com/0Dw4dCP.png\" width=\"379\" height=\"530\">\r\n"
+"</body></html>\r\n"; /*html website*/
+
+int main(void) {
+  int status;
+  fd_set main_fds, read_fds;  //main fd list && temporary fd list
+  FD_ZERO(&main_fds); FD_ZERO(&read_fds);
+  char buf[4096];
+  struct addrinfo hints;
+  struct addrinfo *servinfo, *ptr; // point to the result and a addrinfo pointer
+  memset(&hints, 0, sizeof(hints)); // ensure the struct is empty
+  hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  hints.ai_flags = AI_PASSIVE; // fill up my IP 
+  /* Translate name of a service location and/or a service name to set of
+   socket addresses.
+
+   This function is a possible cancellation point and therefore not
+   marked with __THROW.  */
+  if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    exit(1);
+  }
+  int listen_fd, optval = 1;
+  for (ptr = servinfo; ptr != NULL; ptr = ptr->ai_next) {
+    /* Create a new socket of type TYPE in domain DOMAIN, using
+   protocol PROTOCOL.  If PROTOCOL is zero, one is chosen automatically.
+   Returns a file descriptor for the new socket, or -1 for errors.  */
+    if ((listen_fd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol)) < 0) { //set socket
+      perror("socket error:");
+      continue;
+    }
+    /* Set socket FD's option OPTNAME at protocol level LEVEL
+   to *OPTVAL (which is OPTLEN bytes long).
+   Returns 0 on success, -1 for errors.  */
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) < 0) { //set socket option
+      perror("set socket option error:");
+      exit(1);
+    }
+    /* Give the socket FD the local address ADDR (which is LEN bytes long).  */
+    if (bind(listen_fd, ptr->ai_addr, ptr->ai_addrlen) < 0) {
+      perror("bind error:");
+      close(listen_fd);
+      continue;
+    }
+    break;
+  }
+  if (ptr == NULL) {  //failed to bind
+    fprintf(stderr, "elect_server: failed to bind\n");
+    exit(2);
+  }
+  freeaddrinfo(servinfo);
+  /* Prepare to accept connections on socket FD.
+   N connection requests will be queued before further requests are refused.
+   Returns 0 on success, -1 for errors.  */
+  if (listen(listen_fd, RESTRICT) == -1) {
+    perror("listen error");
+    exit(3);
+  }
+  FD_SET(listen_fd, &main_fds);
+  int max_fd = listen_fd;
+
+  while (1) {
+    fd_set read_fds = main_fds;
+    struct timeval tv;
+    tv.tv_sec = 5; tv.tv_usec = 0;
+    int ret = 0, i = 0;
+    /* Check the first NFDS descriptors each in READFDS (if not NULL) for read
+   readiness, in WRITEFDS (if not NULL) for write readiness, and in EXCEPTFDS
+   (if not NULL) for exceptional conditions.  If TIMEOUT is not NULL, time out
+   after waiting the interval specified therein.  Returns the number of ready
+   descriptors, or -1 for errors.
+
+   This function is a cancellation point and therefore not marked with
+   __THROW.  */
+    if ((ret = select(max_fd + 1, &read_fds, NULL, NULL, &tv)) == -1) {
+      perror("select error:");
+      exit(4);
+    } else if (ret == 0) {
+      printf("Time out!\n");
+      continue;
+    } else {
+      for (i = 0; i < max_fd; i++) {
+        if (FD_ISSET(i, &read_fds)) {
+          if (i == listen_fd) {  //new connected
+            struct sockaddr_in client_addr;
+            int new_fd;
+            socklen_t socklen = sizeof(struct sockaddr_in);
+            /* Await a connection on socket FD.
+            When a connection arrives, open a new socket to communicate with it,
+            set *ADDR (which is *ADDR_LEN bytes long) to the address of the connecting
+            peer and *ADDR_LEN to the address's actual length, and return the
+            new socket's descriptor, or -1 for errors.
+
+            This function is a cancellation point and therefore not marked with
+            __THROW.  */
+            if ((new_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &socklen) == -1)) {
+              perror("accept error:");
+              exit(5);
+            } else {
+              memset(buf, '\0', sizeof(buf));
+              read(new_fd, buf, sizeof(buf) - 1);
+              printf("New connection comes from %s on PORT:%s by fd:%d\n", 
+                      inet_ntoa(client_addr.sin_addr), PORT, new_fd);
+              write(new_fd, website, sizeof(website) - 1);
+            }
+          } else { // already connected
+            int recv_len;
+            memset(buf, '\0', sizeof(buf));
+            //recv() is used to receive messages from a connected socket//
+            if ((recv_len = recv(i, buf, sizeof(buf), 0)) == -1) {
+              perror("recv error:");
+              exit(6);
+            } else if (recv_len == 0) {
+              printf("Disconnected\n");
+            } else {
+              printf("Receive message:%s\n", buf);
+              send(i, buf, recv_len, 0);
+            }
+            close(i);
+            FD_CLR(i, &main_fds);
+          }
+        }
+      }
+    }
+  } 
+  return 0;  
+}
